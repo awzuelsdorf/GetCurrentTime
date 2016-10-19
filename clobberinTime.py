015b712592eca7aad2198489c9c7d0897fdff1a9
Andrew Zuelsdorf <azuelsdorf16@gmail.com>
2016-10-19 19:10:21 -0400

    Made sure that geocode exceptions are caught, added example usages.

--- clobberinTime.py
+++ clobberinTime.py
@@ -1,5 +1,16 @@
 #! /usr/bin/env python3
 
+#Copyright 2016 Andrew Zuelsdorf. All rights reserved.
+
+#This program takes, on the command line, a city,
+#an optional state, and an optional country. Its output
+#is the current time in that city or an error message
+#if no such time can be found. The user is not guaranteed to get
+#an error message if she enters an invalid city
+#(such as Noplaceville, NY, USA). But if she enters a valid city,
+#then she will get a valid time assuming no network
+#issues and no issues with the remote web service.
+
 import geopy, datetime, sys
 
 def main():
@@ -10,7 +21,10 @@ def main():
 			progName = sys.argv[0]
 		else:
 			progName = "(program name)"
-		sys.stderr.write("Usage: {0} (city name) [state/province name] (country name)\n".format(progName))
+		sys.stderr.write("Usage: {0} (city name) [state/province name] [country name]\n".format(progName))
+		sys.stderr.write("Example: {0} \"Albany\" \"New York\" \"USA\"\n".format(progName))
+		sys.stderr.write("Example: {0} \"Nairobi\" \"Kenya\"\n".format(progName))
+		sys.stderr.write("Example: {0} \"Tokyo\"\n".format(progName))
 		return
 
 	geolocator = geopy.geocoders.GoogleV3()
@@ -23,9 +37,11 @@ def main():
 	except geopy.exc.GeocoderServiceError as gcse:
 		sys.stderr.write("ERROR: Couldn't find the current time for \"{0}\". Please check your Internet connection and then try again.\n".format(city))
 		return
+	except:
+		sys.stderr.write("ERROR: Couldn't find the current time for \"{0}\". Please try again.\n".format(city))
+		return
 
 	timeZone = geolocator.timezone((latitude, longitude))
-	print(timeZone)
 	sys.stdout.write("{0}\n".format(datetime.datetime.now(timeZone)))
 
 if __name__ == "__main__":
